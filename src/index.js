e5e22be74c98eedccf01756d8fb84b0a25aa4b30
Mohammad Al Meyzad <48017735+almezyad90@users.noreply.github.com>
2020-01-19 21:12:59 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>hello</h1>
+    <ul>
+      <li>tomato</li>
+      <li>tomato</li>
+      <li>tomato</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
