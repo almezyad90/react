c21b1b1eadcbc5ae02fe9d614558d5067f65e645
Mohammad Al Meyzad <48017735+almezyad90@users.noreply.github.com>
2020-01-20 15:36:38 +0100

    new
    new

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+/*const age = 25;
+
 ReactDOM.render(
   <div>
-    <h1>hello</h1>
+        <h1>hello my age is {age} or { Math.floor(Math.random() * 10)}</h1>
     <ul>
       <li>tomato</li>
       <li>tomato</li>
       <li>tomato</li>
     </ul>
+
+    <h1>hello my age is {`${age}`} </h1>
+
+  
   </div>,
   document.getElementById("root")
-);
+);*/
+
